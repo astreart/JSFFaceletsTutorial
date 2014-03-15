3a10084079b785971af2c3da8400f424cd7da4c4
Astreart <astreart@gmail.com>
2014-03-15 13:00:37 +0200

    Problems wiht Github

---
+++ src/model/TFilterType.java
@@ -0,0 +1,118 @@
+package model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * Номенклатура на типа филтър (възможни стойности: цена, брой гости и други)
+ */
+@Entity
+@Table(name = "T_FILTER_TYPE")
+public class TFilterType implements java.io.Serializable {
+
+	/**
+	 * ИД на филтър
+	 */
+	private Long id;
+
+	/**
+	 * Наименование на вида филтър
+	 */
+	private String filterTypeName;
+
+	/**
+	 * Описание на вида филтър
+	 */
+	private String filterTypeDesc;
+
+	private Set<TRequestFilter> tRequestFilters = new HashSet<TRequestFilter>(0);
+
+	public TFilterType() {
+	}
+
+	public TFilterType(Long id, String filterTypeName, String filterTypeDesc) {
+		this.id = id;
+		this.filterTypeName = filterTypeName;
+		this.filterTypeDesc = filterTypeDesc;
+	}
+
+	public TFilterType(Long id, String filterTypeName, String filterTypeDesc,
+			Set<TRequestFilter> tRequestFilters) {
+		this.id = id;
+		this.filterTypeName = filterTypeName;
+		this.filterTypeDesc = filterTypeDesc;
+		this.tRequestFilters = tRequestFilters;
+	}
+	
+	@Id
+	@Column(name = "ID", unique = true, nullable = false)
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "FILTER_TYPE_NAME", nullable = false, length = 50)
+	public String getFilterTypeName() {
+		return this.filterTypeName;
+	}
+
+	public void setFilterTypeName(String filterTypeName) {
+		this.filterTypeName = filterTypeName;
+	}
+	
+	@Column(name = "FILTER_TYPE_DESC", nullable = false, length = 200)
+	public String getFilterTypeDesc() {
+		return this.filterTypeDesc;
+	}
+
+	public void setFilterTypeDesc(String filterTypeDesc) {
+		this.filterTypeDesc = filterTypeDesc;
+	}
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "filterType")
+	public Set<TRequestFilter> getReqFilters() {
+		return this.tRequestFilters;
+	}
+
+	public void setReqFilters(Set<TRequestFilter> tRequestFilters) {
+		this.tRequestFilters = tRequestFilters;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TFilterType other = (TFilterType) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+
+
+}
---
+++ src/model/TRequestFilter.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * Таблица, пазеща информация за видовете филтри, свързани с дадено запитване за организиране на събитие
+ */
+
+@Entity
+@Table(name = "T_REQUEST_FILTER")
+public class TRequestFilter implements java.io.Serializable {
+
+	/**
+	 * ИД на филтър
+	 */
+	private Long id;
+
+}
