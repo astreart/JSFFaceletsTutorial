26c6f676f8f9c0e59072f049f5d61d26247f29c9
Astreart <astreart@gmail.com>
2014-04-25 11:55:05 +0300

    Improve ListAgencies in Toggleable fieldset

--- src/bg/fmi/master/thesis/beans/AgencyBean.java
+++ src/bg/fmi/master/thesis/beans/AgencyBean.java
@@ -4,25 +4,27 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 import bg.fmi.master.thesis.model.TAgency;
-import bg.fmi.master.thesis.model.TEventType;
-import bg.fmi.master.thesis.model.TUser;
 import bg.fmi.master.thesis.util.HibernateUtil;
 
 @ManagedBean(name = "agencyBean")
-@SessionScoped
+@ViewScoped
 public class AgencyBean {
 
-	private TAgency tEventType = new TAgency();
+	private TAgency tAgency = new TAgency();
+	private List<TAgency> listAgencies;
 
-	public List<TAgency> listAgencies() {
+	/*public List<TAgency> listAgencies() {
 		EntityManager em = HibernateUtil.getEntityManager();
 		// read the existing entries and write to console
 		Query q = em.createQuery("select u from TAgency u");
@@ -33,12 +35,38 @@ public class AgencyBean {
 		}
 		return agencyList;
 	}
+*/
 
-	public TAgency gettEventType() {
-		return tEventType;
+	@PostConstruct
+	public void init(){
+		EntityManager em = HibernateUtil.getEntityManager();
+		// read the existing entries and write to console
+		Query q = em.createQuery("select u from TAgency u");
+		listAgencies = q.getResultList();
+		for (TAgency agency : listAgencies) {
+			System.out.println("NAme: " + agency.gettUser().getName());
+
+		}
+	}
+	
+	public TAgency gettAgency() {
+		return tAgency;
 	}
 
-	public void settEventType(TAgency tEventType) {
-		this.tEventType = tEventType;
+
+	public void settAgency(TAgency tAgency) {
+		this.tAgency = tAgency;
 	}
+
+
+	public List<TAgency> getListAgencies() {
+		return listAgencies;
+	}
+
+
+	public void setListAgencies(List<TAgency> listAgencies) {
+		this.listAgencies = listAgencies;
+	}
+
+	
 }
