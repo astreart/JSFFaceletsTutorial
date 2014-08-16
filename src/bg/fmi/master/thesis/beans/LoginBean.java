abcd605001d4133e07716058156c143d75c75ed4
Astreart <astreart@gmail.com>
2014-08-16 22:04:54 +0300

    Login

--- src/bg/fmi/master/thesis/beans/LoginBean.java
+++ src/bg/fmi/master/thesis/beans/LoginBean.java
@@ -8,34 +8,37 @@ package bg.fmi.master.thesis.beans;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-@ManagedBean(name="loginBean")
+@ManagedBean(name = "loginBean")
 @SessionScoped
-public class LoginBean
-{
-    private String name;
-    private String password;
+public class LoginBean {
+	private String name;
+	private String password;
 
+	public String getName() {
+		return name;
+	}
 
-    public String getName ()
-    {
-        return name;
-    }
+	public void setName(final String name) {
+		this.name = name;
+	}
 
+	public String getPassword() {
+		return password;
+	}
 
-    public void setName (final String name)
-    {
-        this.name = name;
-    }
+	public void setPassword(final String password) {
+		this.password = password;
+	}
 
+	public String logout() {
+		this.name = null;
+		this.password = null;
+		return "logout";
+	}
 
-    public String getPassword ()
-    {
-        return password;
-    }
-
-
-    public void setPassword (final String password)
-    {
-        this.password = password;
-    }
+	public String login(String name, String password) {
+		this.name = name;
+		this.password = password;
+		return "login";
+	}
 }
